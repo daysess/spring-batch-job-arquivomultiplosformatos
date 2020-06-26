3d46f20c794a149237289555a0ac0f1707a61d80
Dayse Soares <daysedesign@gmail.com>
2020-06-25 22:24:21 -0300

    Projeto spring batch - job para ler arquivo com formato multiplos

---
+++ src/main/java/com/springbatch/arquivomultiplosformatos/reader/ArquivoMultiplosFormatosReaderConfig.java
@@ -0,0 +1,15 @@
+package com.springbatch.arquivomultiplosformatos.reader;
+
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ArquivoMultiplosFormatosReaderConfig {
+
+	@Bean
+	public FlatFileItemReader arquivoMultiplosFormatosItemReader() {
+		return null;
+	}
+
+}
